2887587ff9efa8ed1f0298ba2368a1a4c8a4bcaf
Zillion <zillion.h@gmail.com>
2015-05-22 16:27:13 +0800

    ignore bookmarks

--- init.el
+++ init.el
@@ -188,7 +188,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bmkp-last-as-first-bookmark-file "/home/bodhi/.emacs.d/bookmarks")
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
  '(session-use-package t nil (session)))
 ;;; Local Variables:
 ;;; no-byte-compile: t
